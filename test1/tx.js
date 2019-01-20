55850b2ef32e81283f32bc16ff73f6b6bde75c6d
wanify <etelmok@gmail.com>
2019-01-20 14:22:54 +0900

    Init database branch

--- test1/tx.js
+++ test1/tx.js
@@ -1,15 +1,9 @@
 const mongo = require('mongodb').MongoClient
 const url = 'mongodb://localhost:27017'
-mongo.connect(url, {useNewUrlParser: true }, (err, client) => {
-    if (err) {
-      console.error(err)
-      return
-    }
-
+mongo.connect(url, {useNewUrlParser: true })
+.then((client) => {
     //초기화
-    client.db('tx_db').dropDatabase()
-
-
+    client.db('tx_db').dropDatabase();
     //tx database 생성
     const tx_db = client.db('tx_db')
 
@@ -21,22 +15,31 @@ mongo.connect(url, {useNewUrlParser: true }, (err, client) => {
     
     //make POA's tx collection(from, prev_bal, after_bal, amount, to)
     const tx_POA = tx_db.collection('tx_POA')
-    tx_POA.insertMany(txList, (err,result) => {})
+    tx_POA.insertMany(txList)
+    .then(
+        (err, result) => {
+            console.log(result);
+            const tx_user1 = tx_db.collection('tx_user1')
+            let user1_tx = tx_POA.find({ to: 'user1' });
+            tx_user1.insertMany( user1_tx )
+            .then((err, res) => {
+                tx_user1.find().toArray((err,items) => {console.log(items)})
+            })
+        }
+    );
 
-    // show all tx (POA's tx collection)
-    tx_POA.find().toArray((err,items) => {console.log(items)})
+    // // show all tx (POA's tx collection)
+    // tx_POA.find().toArray((err,items) => {console.log(items)})
 
     //make tx_user1's tx collection -Todo
-    const tx_user1 = tx_db.collection('tx_user1')
-    tx_POA.find({to:'user1'}).forEach(
-        function(doc, err){
-            tx_user1.insertOne(doc, err =>{})
-        }
-    )
+    // const tx_user1 = tx_db.collection('tx_user1')
+    // let user1_tx = tx_POA.find({ to: 'user1' });
+    // tx_user1.insertMany( user1_tx );
+    
+    
 
     //show user1's tx
-    tx_user1.find().toArray((err,items) => {console.log(items)})
+    
 
-    client.close()
-
-})
+    client.close();
+});
