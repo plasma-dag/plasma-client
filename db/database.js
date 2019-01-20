2ed5c2fdab693db22a2712b7e01e717071f4687f
wanify <etelmok@gmail.com>
2019-01-20 15:45:48 +0900

    add writeBlock()

--- db/database.js
+++ db/database.js
@@ -1,1 +1,41 @@
 // levelDB, DB related APIs...
+'use-strict';
+const mongo = require('mongodb').MongoClient
+const url = 'mongodb://localhost:27017'
+
+/**
+ * 
+ */
+class Database{ 
+    constructor() {
+        this.db = mongo;
+
+    }
+    
+    readBlock() {
+
+    }
+    
+    writeBlock(block) {
+        return new Promise((resolve, reject) => {
+            this.db.connect(url, {useNewUrlParser: true }, (err, client) => {
+                if (err) {
+                  console.error(err)
+                  return
+                }
+                block._id = block.hash();
+    
+                const blocks = client.db('plasma').collection('blocks')
+                
+                blocks.insertOne(block)
+                    .then(({result}) => resolve(result))
+                    .catch(err => reject(err));
+                }
+            )
+        });
+    }
+}
+
+module.exports = {
+    Database
+};
---
+++ db/database_test.js
@@ -0,0 +1,21 @@
+'use-strict';
+const { Block, Header } = require('../core/block');
+const { Database } = require('./database');
+
+let newHeader = new Header(
+    'dddddddddd',
+    null,
+    null,
+    0,
+    12,
+    12314124,
+    152,
+    null
+);
+
+let newBlock = new Block(newHeader, 'signature', []);
+
+const db = new Database();
+
+db.writeBlock(newBlock).then( res => console.log(res) );
+
