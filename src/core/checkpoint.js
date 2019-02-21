6f92bc52c59c1992be68e9b974cb692f3d71fe9e
wan ps <etelmok@gmail.com>
2019-02-22 02:13:08 +0900

    add documentation for checkpoint.js & potential.js for sphinx documentation

--- src/core/checkpoint.js
+++ src/core/checkpoint.js
@@ -1,13 +1,19 @@
 "use-strict";
 const { makeSignature, hashMessage, ecrecover } = require("../crypto");
 
+/**
+ * Represents Checkpoint structure
+ * Checkpoint is the finalization of a block by the Operator.
+ * Operator finalizes a block by making a checkpoint with the block's hash, with his/her signature.
+ */
+
 class Checkpoint {
   /**
    * @constructor
    *
-   * @param {Address} address
-   * @param {Hash}    blockHash
-   * @param {Number}  operatorNonce
+   * @param {Address} address         User's address
+   * @param {Hash}    blockHash       Block Hash of a block which is finalized by Operator
+   * @param {Number}  operatorNonce   Nonce that Operator uses to order checkpoints
    */
   constructor(address, blockHash, operatorNonce, r, s, v) {
     this.address = address;
@@ -28,6 +34,10 @@ class Checkpoint {
     return this.opHash;
   }
 
+  /**
+   * Gets a signature and stores the r,s,v value in the object
+   * @param {Signature} sig
+   */
   withSignature(sig) {
     this.r = sig.r;
     this.s = sig.s;
@@ -35,12 +45,21 @@ class Checkpoint {
     return;
   }
 
+  /**
+   * Check if the signature(r,s,v) of the checkpoint is correct
+   * @param {Address} opAddr
+   */
   validate(opAddr) {
     if (!(this.r && this.s && this.v)) return { error: true };
     return { error: ecrecover(this.hash, this.r, this.s, this.v) !== opAddr };
   }
 }
 
+/**
+ * Makes a signature of Checkpoint with private key, and stores r,s,v of the signature
+ * @param {Checkpoint} cp
+ * @param {String} prv
+ */
 function signCheckpoint(cp, prv) {
   const data = {
     address: cp.address,
